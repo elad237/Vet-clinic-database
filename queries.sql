be31de6beef1e65233e5a1037b52265db2c24279
elad237 <tumbu.elad@gmail.com>
2023-04-08 00:52:13 +0100

    Updated the schema.sql, data.sql and queries.sql files

--- queries.sql
+++ queries.sql
@@ -56,3 +56,47 @@ SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+-- What animals belong to Melody Pond?
+SELECT A.name 
+FROM animals A
+JOIN owners O 
+ON A.owner_id = O.id AND O.full_name = 'Melody Pond';
+-- List of all animals that are pokemon (their type is Pokemon).
+SELECT A.name, S.name
+FROM animals A
+JOIN species S
+ON A.species_id = S.id AND S.name = 'Pokemon';
+-- List all owners and their animals, remember to include those that don't own any animal.
+SELECT O.full_name, A.name
+FROM owners O
+LEFT JOIN animals A
+ON A.owner_id = O.id;
+-- How many animals are there per species?
+SELECT S.name, COUNT(A.id)
+FROM animals A
+JOIN species S
+ON A.species_id = S.id 
+GROUP BY S.name;
+-- List all Digimon owned by Jennifer Orwell.
+SELECT A.name
+FROM animals A
+JOIN owners O
+ON A.owner_id = O.id AND O.full_name = 'Jennifer Orwell'
+JOIN species S
+ON S.id = A.species_id AND s.name = 'Digimon';
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT A.name
+FROM animals A
+JOIN owners O
+ON A.owner_id = O.id 
+AND O.full_name = 'Dean Winchester'
+AND escape_attempts = 0;
+-- Who owns the most animals?
+SELECT O.full_name, COUNT(A.id)
+FROM animals A
+JOIN owners O
+ON A.owner_id = O.id
+GROUP BY O.full_name
+ORDER BY COUNT(A.id) DESC
+LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -11,3 +11,30 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(250);
+
+CREATE TABLE owners(
+	id			INT GENERATED ALWAYS AS IDENTITY,
+	full_name	VARCHAR(250),
+	age			INT
+);
+
+CREATE TABLE species(
+	id			INT GENERATED ALWAYS AS IDENTITY,
+	name		VARCHAR(250)
+);
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE species ADD CONSTRAINT uniq_species_id_constraint PRIMARY KEY(id);
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE owners ADD CONSTRAINT uniq_owner_id_constraint PRIMARY KEY(id);
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
